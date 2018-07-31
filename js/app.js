6e00b7581a29a94a72ee156d6f3af3f247c258f2
Suzanne Su <suhysu@gmail.com>
2018-07-31 14:59:45 -0700

    added guess name question

--- js/app.js
+++ js/app.js
@@ -11,3 +11,11 @@ if (greetingsResponse === yes) {
 } else {(greetings !== yes);
   alert('oh come on, it will be fun');
 }
+var guessName = prompt ('Is my name Suzanne?');
+var guessNameLower = guessName.toLowerCase();
+console.log ('Q: Is my name Suzanne? A: ' + guessNameLower);
+if (guessNameLower === yes) {
+  alert ('Great job! That\'s me');
+} else { (guessNameLower !== yes);
+  alert ('Incorrect. My name really is Suzanne');
+}
